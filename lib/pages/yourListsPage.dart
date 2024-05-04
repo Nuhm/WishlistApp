7940c8de9cde67e9e7816a1ac620187f97c1f674
Mikey <michael.knox@talktalk.net>
2024-05-04 18:40:17 +0100

    Added List Deletion
    Users can now swipe on a list to delete it with a confirmation popup.

--- lib/pages/yourListsPage.dart
+++ lib/pages/yourListsPage.dart
@@ -6,7 +6,7 @@ import 'package:wishlist_app/models/wishlist.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class YourListsPage extends StatefulWidget {
-  const YourListsPage({super.key});
+  const YourListsPage({Key? key}) : super(key: key);
 
   @override
   _YourListsPageState createState() => _YourListsPageState();
@@ -26,22 +26,13 @@ class _YourListsPageState extends State<YourListsPage> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String>? jsonStringList = prefs.getStringList('wishlists');
     if (jsonStringList == null) return;
-    print(jsonStringList);
     List<Wishlist> wishlists = jsonStringList
         .map((jsonString) => Wishlist.fromJson(json.decode(jsonString)))
         .toList();
     setState(() {
       _wishlists = wishlists;
     });
-    }
-
-  // Method to parse JSON string to Wishlist objects
-  List<Wishlist> _parseWishlists(String jsonString) {
-    Iterable decodedList = json.decode(jsonString);
-    List<Wishlist> wishlists = decodedList.map((json) => Wishlist.fromJson(json)).toList();
-    return wishlists;
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -64,18 +55,57 @@ class _YourListsPageState extends State<YourListsPage> {
         itemCount: _wishlists.length,
         itemBuilder: (context, index) {
           Wishlist wishlist = _wishlists[index];
-          return ListTile(
-            title: Text(wishlist.name),
-            subtitle: Text(wishlist.description),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => WishlistPage(wishlist: wishlist),
-                ),
+          return Dismissible(
+            key: Key(wishlist.name), // Unique key for each list item
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.only(right: 20.0),
+              child: const Icon(Icons.delete, color: Colors.white),
+            ),
+            confirmDismiss: (direction) async {
+              // Show a confirmation dialog before deletion
+              return await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("Confirm"),
+                    content: const Text("Are you sure you want to delete this list?"),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(false),
+                        child: const Text("CANCEL"),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(true),
+                        child: const Text("DELETE"),
+                      ),
+                    ],
+                  );
+                },
               );
             },
-
+            onDismissed: (direction) {
+              // Remove the list item from the list
+              setState(() {
+                _wishlists.removeAt(index);
+              });
+              // Update SharedPreferences to reflect the change
+              _updateWishlistsInSharedPreferences();
+            },
+            child: ListTile(
+              title: Text(wishlist.name),
+              subtitle: Text(wishlist.description),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => WishlistPage(wishlist: wishlist),
+                  ),
+                );
+              },
+            ),
           );
         },
       ),
@@ -90,5 +120,11 @@ class _YourListsPageState extends State<YourListsPage> {
         return NewListPage();
       },
     );
+  }
+
+  void _updateWishlistsInSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> jsonStringList = _wishlists.map((wishlist) => json.encode(wishlist.toJson())).toList();
+    prefs.setStringList('wishlists', jsonStringList);
   }
 }
